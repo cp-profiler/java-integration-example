1929f38f99aa9974e72ed69b375e81bb47b66078
Maxim Shishmarev <maxim.shishmarev@monash.edu>
2015-10-21 16:33:21 +1100

    changed to work with new syntax

--- src/main/java/com/github/cpprofiler/Example.java
+++ src/main/java/com/github/cpprofiler/Example.java
@@ -1,21 +1,37 @@
 package com.github.cpprofiler;
 
 import com.github.cpprofiler.Connector;
-import com.github.cpprofiler.Message.Node;
 import org.zeromq.ZMQ;
 
 public class Example {
   public static void main (String args[]) {
 
-    Connector connector = new Connector();
+    Connector c = new Connector();
 
-    connector.connectToSocket(6565);
-    connector.restartGist(-1);
+    /// Connect to port 6565 (default for cp-profiler)
+    c.connect(6565);
 
-    connector.sendNode(0, -1, -1, 2, Node.NodeStatus.BRANCH, "root", "");
-    connector.sendNode(1, 0, 0, 0, Node.NodeStatus.SOLVED, "a", "");
-    connector.sendNode(2, 0, 1, 0, Node.NodeStatus.FAILED, "b", "");
+    /// Starting a new tree (also used in case of a restart)
+    c.restart();
 
-    connector.disconnectFromSocket();
+
+    /// SEND ROOT NODE
+    /// Arguments:
+    /// 1. 0 -- node index;
+    /// 2. -1 -- parent's index (-1 indicates that the parent does not exist)
+    /// 3. -1 -- parent's current alternative (normally starts with 0, but -1 as irrelevant here)
+    /// 4.  2 -- number of children nodes (as in binary tree)
+    /// 5.  Connector.NodeStatus.BRANCH -- decision node / node with children
+    /// The rest of fields are optional and can be specified in arbitrary order
+    c.createNode(0, -1, -1, 2, Connector.NodeStatus.BRANCH).send();
+
+    // SEND LEFT CHILD
+    c.createNode(1, 0, 0, 0, Connector.NodeStatus.SOLVED).setLabel("a").send();
+
+    // SEND RIGHT CHILD
+    // "Info" field is specified, which is arbitrary text to go along with the node
+    c.createNode(2, 0, 1, 0, Connector.NodeStatus.FAILED).setLabel("b").setInfo("some info").send();
+
+    c.disconnect();
   }
 }
